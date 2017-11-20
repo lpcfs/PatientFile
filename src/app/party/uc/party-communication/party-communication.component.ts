4d2131086370b3de6303c3af0b304f26588c230d
Luc <luc.peeters@portima.com>
2017-11-20 18:02:41 +0100

    extract form model

--- src/app/party/uc/party-communication/party-communication.component.ts
+++ src/app/party/uc/party-communication/party-communication.component.ts
@@ -1,63 +1,15 @@
 
-import { Component, OnInit, Inject, Output } from '@angular/core';
+import { Component, OnInit, Inject, Input, Output } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { PatientId, Patient, communication } from '../../patient/patient';
 import { Observable } from 'rxjs/Observable';
 import { EmailValidator } from '@angular/forms';
-
-import {
-  DynamicFormControlModel, DynamicFormService,
-  DynamicCheckboxModel,
-  DynamicInputModel,
-  DynamicRadioGroupModel,
-  DynamicFormGroupModel,
-  DynamicTextAreaModel,
-  DynamicTimePickerModel,
-  DynamicCheckboxGroupModel,
-  DynamicDatePickerModel,
-  DynamicSelectModel
-} from "@ng-dynamic-forms/core";
-
-export const MY_FORM_MODEL: DynamicFormControlModel[] = [
-
-  new DynamicInputModel(
-    {
-      id: "email",
-      placeholder: "Email",
-      validators: {
-        required: null
-      },
-      errorMessages: {
-        required: "{{ placeholder }} is verplicht"
-      }
-    }
-  ),
-  new DynamicInputModel(
-    {
-      id: "phone",
-      placeholder: "Telefoon",
-      validators: {
-        required: null
-      },
-      errorMessages: {
-        required: "{{ placeholder }} is verplicht"
-      }
-    }
-  ),
-  new DynamicInputModel(
-    {
-      id: "phoneEmengency",
-      placeholder: "Telefoon bij noodgevallen",
-      validators: {
-      }
-    }
-  )
-
-];
+import { MY_FORM_MODEL } from './party-communication.formModel';
+import { DynamicFormService, DynamicFormControlModel } from '@ng-dynamic-forms/core';
+import { GeneratedFile } from '@angular/compiler';
 
 
 @Component({
@@ -73,27 +25,34 @@ export class PartyCommunicationComponent implements OnInit {
   formGroup: FormGroup;
 
   id: string;
+  query_path: string;
 
   communicationDoc: AngularFirestoreDocument<communication>;
   communication: Observable<communication>;
   communicationOriginal: communication;
 
-  constructor(private route: ActivatedRoute,
-    private router: Router,
+
+
+  constructor( 
     @Inject(FormBuilder) private fb: FormBuilder,
-    public dialog: MatDialog,
-    private afs: AngularFirestore,
-    private formService: DynamicFormService
-  ) {
+    @Inject(ActivatedRoute) private route: ActivatedRoute,
+    @Inject(Router) private router: Router,
+    @Inject(AngularFirestore) private afs: AngularFirestore,
+    @Inject(DynamicFormService) private formService: DynamicFormService) {
+
     // https://coryrylan.com/blog/angular-form-builder-and-validation-management
-    this.formGroup = this.formService.createFormGroup(this.formModel);
+
   }
 
   ngOnInit() {
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+    
     this.id = this.route.snapshot.paramMap.get('id');
 
+    this.query_path = `patient/${this.id}/communication/1`;
+    
     // --- patient communication
-    this.communicationDoc = this.afs.doc<communication>(`patient/${this.id}/communication/1`);
+    this.communicationDoc = this.afs.doc<communication>(this.query_path);
     this.communication = this.communicationDoc.valueChanges();
 
     this.communication.subscribe((v: communication) => {
@@ -124,6 +83,69 @@ export class PartyCommunicationComponent implements OnInit {
 
   cancel() {
     this.formGroup.reset(this.communicationOriginal);
+  }
+
+};
+
+
+class GenericFormComponent<T> implements OnInit {
+
+  public entity_id: string;
+  @Input() query_path: string;
+
+  @Output() summary: string
+
+  formModel: DynamicFormControlModel[] = MY_FORM_MODEL;
+  formGroup: FormGroup;
+  
+  protected firestoreDocument: AngularFirestoreDocument<T>;
+  protected observableOf: Observable<T>;
+  protected original: T;
+
+  constructor(   
+    @Inject(FormBuilder) private fb: FormBuilder,
+    @Inject(ActivatedRoute) private route: ActivatedRoute,
+    @Inject(Router) private router: Router,
+    @Inject(AngularFirestore) private afs: AngularFirestore,
+    @Inject(DynamicFormService) private formService: DynamicFormService) {
+    // https://coryrylan.com/blog/angular-form-builder-and-validation-management
+  }
+
+  ngOnInit() {
+    this.formGroup = this.formService.createFormGroup(this.formModel);
+
+    this.entity_id = this.route.snapshot.paramMap.get('id');
+
+    // --- patient communication
+    this.firestoreDocument = this.afs.doc<T>(`patient/${this.entity_id}/communication/1`);
+    this.observableOf = this.firestoreDocument.valueChanges();
+
+    this.observableOf.subscribe((v: T) => {
+      if (!v) return;
+      this.original = v;
+
+      //this.patientForm.patchValue(v);
+      this.formGroup.reset(this.original);
+    });
+  }
+
+  hotkeys(event) {
+    if (event.keyCode == 83 && event.ctrlKey) {
+      this.save();
+      return false;
+    }
+  }
+
+
+  save() {
+    if (this.formGroup.status != "VALID") return;
+    this.firestoreDocument.set(this.formGroup.value).then(() => {
+
+    })
+  }
+
+  cancel() {
+    this.formGroup.reset(this.original);
   }
 
 };
